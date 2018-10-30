fd05dd9c2b63ea599f6beab9ffafe7716a236949
arturo <arturo@MacBook-Pro-2.local>
2018-10-30 16:07:41 -0500

    Modification

--- src/components/ExpenseiveListIten.js
+++ src/components/ExpenseiveListIten.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom'
+import numeral from 'numeral';
 
 const ExpenseListItem = ({ id, description, amount, createdAt }) => (
     <div>
         <Link to={`/edit/${id}`}>
             <h3>{description}</h3>
         </Link>
-        <p>{amount} - {createdAt}</p>
+        <p>{numeral(amount).format('$0,0.00')} - {createdAt}</p>
     </div>
 );
 
